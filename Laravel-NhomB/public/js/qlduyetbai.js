1c5a4a5767aec2d1a84b9126e356ebfeb6910531
tienphat2108 <roghe2026@gmail.com>
2025-05-19 09:40:32 +0700

    Delete Laravel-NhomB directory

--- Laravel-NhomB/public/js/qlduyetbai.js
+++
@@ -1,9 +0,0 @@
-// Ví dụ thêm hiệu ứng khi hover vào nút
-document.querySelectorAll('td').forEach(td => {
-    td.addEventListener('mouseover', () => {
-        td.style.backgroundColor = '#eee';
-    });
-    td.addEventListener('mouseout', () => {
-        td.style.backgroundColor = '';
-    });
-});
--- Laravel-NhomB/resources/views/admin/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.admin')
-@section('content')
-    <div class="container-fluid">
-        <h1 class="h3 mb-4 text-gray-800">Dashboard Admin</h1>
-        <p>Chào mừng bạn đến trang quản trị!</p>
-    </div>
-@endsection 
