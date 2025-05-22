c92116d451dd5fec641ee479116efda39bfffc73
nguyennguyen777 <hanhnguyenhpt1502@gmail.com>
2025-05-22 17:01:52 +0700

    chuc nang xuatdulieu

--- Laravel-NhomB/app/Http/Controllers/Admin/AnalyticsController.php
+++ Laravel-NhomB/app/Http/Controllers/Admin/AnalyticsController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\User;
+use App\Exports\PostsExport;
+use App\Exports\UsersExport;
+use Maatwebsite\Excel\Facades\Excel;
 
 class AnalyticsController extends Controller
 {
@@ -27,6 +31,27 @@ class AnalyticsController extends Controller
     {
         // Tạm thời return view trống, sau này sẽ thêm logic xuất dữ liệu
         return view('admin.xuatdulieu');
+    }
+
+    public function export(Request $request)
+    {
+        $dataType = $request->query('type');
+        $formatType = $request->query('format');
+
+        switch ($dataType) {
+            case 'posts':
+                $exporter = new PostsExport();
+                $filename = 'posts_' . date('Ymd_His') . '.' . $formatType;
+                return Excel::download($exporter, $filename);
+                break;
+            case 'users':
+                $exporter = new UsersExport();
+                 $filename = 'users_' . date('Ymd_His') . '.' . $formatType;
+                return Excel::download($exporter, $filename);
+                break;
+            default:
+                return back()->with('error', 'Loại dữ liệu không hợp lệ.');
+        }
     }
 
     public function performanceReport()
